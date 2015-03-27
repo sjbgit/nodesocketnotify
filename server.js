2b64714b181d6c821bf7984ca8027b615ebb2728
sjbgit <sjb_strat@hotmail.com>
2015-03-27 15:54:32 -0500

    update for perf counters

--- server.js
+++ server.js
@@ -39,6 +39,10 @@ router.get('/api/notify', function (req, res) {
     res.json({message: 'test message from server'}); // return all todos in JSON format
 });
 
+router.post('/api/perftest', function (req, res) {
+    res.json({message: 'Perf test - test message from server'}); // return all todos in JSON format
+});
+
 router.post('/api/perfcounter', function(req, res) {
 
     var body = req.body;
