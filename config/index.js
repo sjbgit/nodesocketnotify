ebd2d42f074be266c81b44d215b4f6ea18ea75d3
sjbgit <sjb_strat@hotmail.com>
2015-04-23 18:25:07 -0500

    added name and environment to redis info

--- config/index.js
+++ config/index.js
@@ -1,4 +1,6 @@
 module.exports = {
+	name: 'bingpulseuatsess01',
+	environment: 'UAT',
 	port: 6379,
 	url: 'bingpulseuatsess01.redis.cache.windows.net',
 	key: 'MDPZiB0HMcMMBU53a2x0nAOjfAUbP8/B4QOztrZwt2w='
