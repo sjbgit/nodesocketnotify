8a1ee5b80babb1cfa50966d47917f20e0f9a313b
sjbgit <sjb_strat@hotmail.com>
2015-06-19 18:52:36 -0500

    update cors

--- server.js
+++ server.js
@@ -135,6 +135,8 @@ router.configure(function () {
 router.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
@@ -203,7 +205,7 @@ router.post('/api/roomsend', function(req, res) {
 
     var body = req.body;
 
-    var message = body.message;  
+    var message = body.message;
 
     console.log('message: ' + message);
 
